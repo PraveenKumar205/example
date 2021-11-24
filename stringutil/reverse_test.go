6230d9d3f63c9d56f7a2016607666a8ab256d355
PraveenKumar205 <praveen.a@shopf.co>
2021-11-24 14:56:00 +0530

    fail a test case

--- stringutil/reverse_test.go
+++ stringutil/reverse_test.go
@@ -44,7 +44,7 @@ func TestDummyfunction(t *testing.T) {
 
 func TestDummyfunction3(t *testing.T) {
 	in := "Hi"
-	want := "Hi"
+	want := "Hey"
 	got := Dummyfunction3(in)
 	if got != want {
 		t.Errorf("Error ")
